d7b52629f6599a9218694c972ad747662a0d61e6
Bulbakov Danila <dbulbakov@bk.ru>
2024-01-13 19:27:40 +0600

    I learnt how to move from one page to other using 'navigate.back()/to()/forward'

--- src/test/java/LessonOne.java
+++ src/test/java/LessonOne.java
@@ -4,10 +4,15 @@ import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class LessonOne {
-WebDriver driver = new ChromeDriver();
+
+    WebDriver driver = new ChromeDriver();
+
     @Test
     public void lessonOne(){
+
         /*WebDriver driver = new ChromeDriver();
         driver.get("https://grinfer.com/");
 
@@ -25,10 +30,32 @@ WebDriver driver = new ChromeDriver();
         driver.close();
         driver.quit();*/
 
-        driver.get("https://github.com/BulbazavrFromRus");
+        /*driver.get("https://github.com/BulbazavrFromRus");
         Cookie getCookie = new Cookie("user_session", "9rwnt_Poklm1pgIHXGdTg3RkRm-SBdNp9lA8qkv1VSRFSyiY");
         driver.manage().addCookie(getCookie);
         driver.navigate().refresh();
+        Set<Cookie> cookieSet =  driver.manage().getCookies();
+        System.out.println("All cookie: "+ cookieSet);
+
+        driver.close();*/
+
+
+
+        driver.get("https://grinfer.com/");
+        driver.navigate().to("https://unsplash.com/");
+
+        driver.navigate().back();
+
+        String urlGrinfer = driver.getCurrentUrl();
+        Assert.assertEquals(urlGrinfer, "https://grinfer.com/");
+
+        String getTitle = driver.getTitle();
+        Assert.assertEquals("Top Online Courses | Grinfer", getTitle);
+
+
+        driver.close();
+
+
 
 
     }
