3b5b22b3d0f5744adceca3e772a212230e823d7c
Bulbakov Danila <dbulbakov@bk.ru>
2024-01-13 18:59:51 +0600

    I could sign in to my github using cookies

--- src/test/java/LessonOne.java
+++ src/test/java/LessonOne.java
@@ -1,13 +1,14 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LessonOne {
-
+WebDriver driver = new ChromeDriver();
     @Test
     public void lessonOne(){
-        WebDriver driver = new ChromeDriver();
+        /*WebDriver driver = new ChromeDriver();
         driver.get("https://grinfer.com/");
 
         String getUrl = driver.getCurrentUrl();
@@ -22,7 +23,13 @@ public class LessonOne {
         System.out.println("Page "+getPage);
 
         driver.close();
-        driver.quit();
+        driver.quit();*/
+
+        driver.get("https://github.com/BulbazavrFromRus");
+        Cookie getCookie = new Cookie("user_session", "9rwnt_Poklm1pgIHXGdTg3RkRm-SBdNp9lA8qkv1VSRFSyiY");
+        driver.manage().addCookie(getCookie);
+        driver.navigate().refresh();
+
 
     }
 }
